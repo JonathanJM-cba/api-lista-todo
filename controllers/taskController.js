04496bddb307f176c22563a9091e569e433d31bb
jonathan <jonathan20cba@gmail.com>
2025-04-28 22:45:19 -0300

    Se agrega endpoint para actualizar tarea

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -12,17 +12,47 @@ const createTask = async (req, res) => {
     };
 
     const newTask = await tasksModel.create(taskData);
-    res
-      .status(201)
-      .json({
-        id: newTask.id,
-        title: newTask.title,
-        description: newTask.description,
-      });
+    res.status(201).json({
+      id: newTask.id,
+      title: newTask.title,
+      description: newTask.description,
+    });
   } catch (error) {
     console.log("Error al intentar crear una nueva tarea: ", error);
     handleHttpError(res, "ERROR_CREATE_TASK", 500);
   }
 };
 
-module.exports = { createTask };
+const updateTask = async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+  const { email } = req.user;
+  try {
+    //Se verifica si existe la tarea
+    const task = await tasksModel.findByPk(id);
+
+    if (!task) return handleHttpError(res, "ERROR_TASK_NOT_FOUND", 404);
+
+    //Se verifica si esa tarea pertenece al usuario correcto
+    if (task.userEmail !== email)
+      return handleHttpError(res, "ERROR_NO_PERMISSIONS_UPDATE_TASK", 403);
+
+    //Caso contrario se actualiza la tarea
+    const taskData = {
+      title: title,
+      description: description,
+    };
+
+    const updatedTask = await task.update(taskData);
+    res.status(200).json({
+      id: updatedTask.id,
+      title: updatedTask.title,
+      description: updatedTask.description,
+    });
+  } catch (error) {
+    console.log("Error al intentar actualizar la tarea: ", error);
+    handleHttpError(res, "ERROR_UPDATE_TASK", 500);
+  }
+};
+
+module.exports = { createTask, updateTask };
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { createTask } = require("../controllers/taskController");
+const { createTask, updateTask } = require("../controllers/taskController");
 const checkAuth = require("../middleware/checkAuth");
 const { validatorCreateTask } = require("../validators/taskValidator");
 const router = express.Router();
 
 router.post("/todos", checkAuth, validatorCreateTask, createTask);
+
+router.put("/todos/:id", checkAuth, validatorCreateTask, updateTask);
 
 module.exports = router;
